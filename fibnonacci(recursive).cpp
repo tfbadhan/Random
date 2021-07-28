015b83451d94ce547ca84f944ea8cbc5da16565e
Toushiful Ferdous Badhan <68116912+tfbadhan@users.noreply.github.com>
2021-07-28 16:39:25 +0600

    Create fibnonacci(recursive).cpp

---
+++ fibnonacci(recursive).cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fib(int n)
+{
+	if(n==0) return 0;
+	if(n==1 || n==2) return 1;
+	
+	else
+	return (fib(n-1) + fib(n-2));
+}
+
+int main()
+{
+	int n;
+	cin>>n;
+	for(int i=0; i<n; i++)
+	{
+		cout<<fib(i)<<" ";
+	}
+	cout<<endl;
+	return 0;
+}
