c102142c9c85b625e7e0b493993fa69590bdf2bb
Toushiful Ferdous Badhan <68116912+tfbadhan@users.noreply.github.com>
2021-07-29 18:37:57 +0600

    Create Digits of Factorial(Kamenetsky formula).cpp

---
+++ Digits of Factorial(Kamenetsky formula).cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int factDigit(int n)
+{
+	if(n<0) return 0;
+	if(n<=1) return 1;
+	
+	double d=((n * log10(n / M_E) + log10(2 * M_PI * n) / 2.0));
+	return floor(d)+1;
+}
+
+int main()
+{
+	int n;
+	cin>>n;
+	cout<<factDigit(n)<<endl;
+	return 0;
+}
