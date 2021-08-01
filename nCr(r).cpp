458b2877423e6ba5c0db6ff50e5bc4ee3aaf3cfb
Toushiful Ferdous Badhan <68116912+tfbadhan@users.noreply.github.com>
2021-08-01 19:22:20 +0600

    Create nCr(r).cpp

---
+++ nCr(r).cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long int nCr(int n, int r)
+{
+	int res=1;
+	
+	if(r>n-r) r=n-r;
+	
+	for(int i=0; i<r; i++)
+	{
+		res*=(n-i);
+		res/=(i+1);
+	}
+	return res;
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n, r;
+    cin>>n>>r;
+    cout<<nCr(n, r)<<endl;
+    return 0;
+}
