cb86c451745cf7d98399016e0f7decb60d2ad4c3
Toushiful Ferdous Badhan <68116912+tfbadhan@users.noreply.github.com>
2021-08-02 22:47:46 +0600

    Create fibonacci Tabulation DP.cpp

---
+++ fibonacci Tabulation DP.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int Tabulation (int n)
+{
+	int f[n+1];
+	f[0]=0;
+	f[1]=1;
+	for(int i=2; i<=n; i++)
+	{
+		f[i]=f[i-1]+f[i-2];
+	}
+	return f[n];
+}
+
+int main()
+{
+	int n;
+	cin>>n;
+	cout<<Tabulation(n)<<endl;
+	return 0;
+}
