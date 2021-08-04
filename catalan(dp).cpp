2ef4569b2963668dbb29d51ec76169aeecbdeaab
Toushiful Ferdous Badhan <68116912+tfbadhan@users.noreply.github.com>
2021-08-04 18:26:47 +0600

    Create catalan(dp).cpp

---
+++ catalan(dp).cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+unsigned long int catalanDP(unsigned int n)
+{
+	unsigned long int catalan[n+1];
+	
+	catalan[0]=catalan[1]=1;
+	for(int i=2; i<=n; i++)
+	{
+		catalan[i]=0;
+		for(int j=0; j<i; j++)
+		{
+			catalan[i]+=catalan[j]*catalan[i-j-1];
+		}
+	}
+	return catalan[n];
+}
+
+int main()
+{
+	int n;
+	cin>>n;
+	for(int i=0; i<n; i++)
+	{
+		cout<<catalanDP(i)<<" ";
+	}
+	return 0;
+}
