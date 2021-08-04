ccccdd4b9ddea783fab9a8e2b438f0adc4c168f1
Toushiful Ferdous Badhan <68116912+tfbadhan@users.noreply.github.com>
2021-08-04 19:45:35 +0600

    Create catalan(binomial).cpp

---
+++ catalan(binomial).cpp
@@ -0,0 +1,37 @@
+//O(n)
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+unsigned long int binomialCoeff(unsigned int n, unsigned int k)
+{
+	unsigned long int res=1;
+	
+	if(k>n-k) k=n-k;
+	
+	for(int i=0; i<k; i++)
+	{
+		res*=(n-i);
+		res/=(i+1);
+	}
+	return res;
+}
+
+unsigned long int catalan(unsigned int n)
+{
+	unsigned long int c= binomialCoeff(2*n, n);
+	
+	return c/(n+1);
+}
+
+int main()
+{
+	int n;
+	cin>>n;
+	for(int i=0; i<n; i++)
+	{
+		cout<<catalan(i)<<" ";
+	}
+	return 0;
+}
